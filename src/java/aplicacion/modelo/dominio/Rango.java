2ddd1317b106a175f6f7e2958be1fa73836357ca
Familia <Familia@DESKTOP-C28KT71>
2017-04-19 20:32:23 -0300

    ratas outputlabel perimetro y superficie final

---
+++ src/java/aplicacion/modelo/dominio/Rango.java
@@ -0,0 +1,92 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion.modelo.dominio;
+
+/**
+ *
+ * @author Franco
+ */
+public class Rango {
+
+    public Rango() {
+    }
+    private int a;
+    private int b;
+    private int primos;
+    private int pares;
+    private int impares;
+   
+    /**
+     * @return the a
+     */
+    public int getA() {
+        return a;
+    }
+    
+    /**
+     * @param a the a to set
+     */
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    /**
+     * @return the b
+     */
+    public int getB() {
+        return b;
+    }
+
+    /**
+     * @param b the b to set
+     */
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    /**
+     * @return the primos
+     */
+    public int getPrimos() {
+        return primos;
+    }
+
+    /**
+     * @param primos the primos to set
+     */
+    public void setPrimos(int primos) {
+        this.primos = primos;
+    }
+
+    /**
+     * @return the pares
+     */
+    public int getPares() {
+        return pares;
+    }
+
+    /**
+     * @param pares the pares to set
+     */
+    public void setPares(int pares) {
+        this.pares = pares;
+    }
+
+    /**
+     * @return the impares
+     */
+    public int getImpares() {
+        return impares;
+    }
+
+    /**
+     * @param impares the impares to set
+     */
+    public void setImpares(int impares) {
+        this.impares = impares;
+    }
+    
+}
